aa8211653b5c4d596565d44809cb4eff777275f6
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-23 22:18:08 -0300

    add: style home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
-
 export default class MyDocument extends Document {
-  render () {
-    return ( 
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-          <title>Ig.news</title>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    ) 
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+                    <title>ig.news</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
