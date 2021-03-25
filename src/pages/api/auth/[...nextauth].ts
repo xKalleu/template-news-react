25074aa90fabc31bd4248fef862d39b6886d9873
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-24 22:15:02 -0300

    add: FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
-import { read } from 'fs/promises';
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 	providers: [
@@ -9,5 +12,30 @@ export default NextAuth({
 			clientSecret: process.env.GITHUB_SECRET,
 			scope: 'read:user'
 		})
-	]
+	],
+	jwt: {
+		signingKey: process.env.SIGNING_KEY
+	},
+	callbacks: {
+		async signIn(user, account, profile) {
+			const { email } = user;
+
+			try {
+				await fauna.query(
+					q.If(
+						q.Not(
+							q.Exists(
+								q.Match(q.Index('user_by_email'), q.Casefold(user.email))
+							)
+						),
+						q.Create(q.Collection('users'), { data: { email } }),
+						q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
+					)
+				);
+				return true;
+			} catch {
+				return false;
+			}
+		}
+	}
 });
