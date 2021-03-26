dad517a15ac3549fe3c5a98eb27982d80230b07f
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-25 22:55:50 -0300

    add: stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,42 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
-    priceId: string;
+	priceId: string;
 }
 
-export function SubscribeButton({priceId}:SubscribeButtonProps ) {
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-        >
-            Subscribe now
-        </button>
-    )
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+	const [session] = useSession();
+	async function handleSubscribe() {
+		if (!session) {
+			signIn('github');
+			return;
+		}
+
+		try {
+			const response = await api.post('/subscribe');
+
+			const { sessionId } = response.data;
+
+			const stripe = await getStripeJs();
+
+			stripe.redirectToCheckout({
+				sessionId
+			});
+		} catch (err) {
+			alert(err.message);
+		}
+	}
+
+	return (
+		<button
+			type="button"
+			onClick={() => handleSubscribe()}
+			className={styles.subscribeButton}>
+			Subscribe now
+		</button>
+	);
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,21 +2,50 @@ import { query as q } from 'faunadb';
 
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
-
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 	providers: [
 		Providers.GitHub({
-			clientId: process.env.GITHUB_ID,
+			clientId: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_SECRET,
 			scope: 'read:user'
 		})
 	],
-	jwt: {
-		signingKey: process.env.SIGNING_KEY
-	},
 	callbacks: {
+		async session(session) {
+			try {
+				const userActiveSubscription = await fauna.query(
+					q.Get(
+						q.Intersection([
+							q.Match(
+								q.Index('subscription_by_user_ref'),
+								q.Select(
+									'ref',
+									q.Get(
+										q.Match(
+											q.Index('user_by_email'),
+											q.Casefold(session.user.email)
+										)
+									)
+								)
+							),
+							q.Match(q.Index('subscription_by_status'), 'active')
+						])
+					)
+				);
+
+				return {
+					...session,
+					activeSubscription: userActiveSubscription
+				};
+			} catch (err) {
+				return {
+					...session,
+					activeSubscription: null
+				};
+			}
+		},
 		async signIn(user, account, profile) {
 			const { email } = user;
 
@@ -32,6 +61,7 @@ export default NextAuth({
 						q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
 					)
 				);
+
 				return true;
 			} catch {
 				return false;
