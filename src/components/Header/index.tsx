680d98473f28446b2715cf57739f050cbd02db5d
Kalleu Oliveira <xkalleu@gmail.com>
2021-03-29 22:56:54 -0300

    add: page post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
 import { SignInButton } from '../SignInButton';
-
 import styles from './styles.module.scss';
 
+import { ActiveLink } from '../ActiveLink';
+
 export function Header() {
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news"/>
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
-                <SignInButton />
-            </div>
-        </header>
-    );
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<img src="/images/logo.svg" alt="ig.news" />
+				<nav>
+					<ActiveLink href="/" activeClassName={styles.active}>
+						<a className={styles.active}>Home</a>
+					</ActiveLink>
+					<ActiveLink href="/posts" activeClassName={styles.active}>
+						<a>Posts</a>
+					</ActiveLink>
+				</nav>
+
+				<SignInButton />
+			</div>
+		</header>
+	);
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 export default class MyDocument extends Document {
 	render() {
 		return (
@@ -9,8 +10,7 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
 						rel="stylesheet"
 					/>
-					<link rel="shortcut icon" href="/favicon.png" type="image/png" />
-					<title>ig.news</title>
+					<link rel="shortcut icon" href="favicon.png" type="image/png" />
 				</Head>
 				<body>
 					<Main />
